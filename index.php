9f99acb813e08cd15b78130e4747810f4f39800e
Ierai <ierai@Ubuntu-12-04.(none)>
2015-12-10 10:55:47 +0100

    Primer commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>E-ontzia</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<img class="logo" src="img/logo.png">
+				
+	</div>
+</body>
+</html>
