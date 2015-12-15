960ed82569b8b3d3f0805f9ff59063fae46b25fe
Ierai Elizondo <ieraielizondo@gmail.com>
2015-12-15 13:59:35 +0100

    slim

--- app/index.php
+++ app/index.php
@@ -2,15 +2,25 @@
 	header ('Content-type: text/html; charset=utf-8');
 	session_start();
 
-	require '../vendor/autoload.php';
+	require 'vendor/autoload.php';
 
-
-	Slim\Slim::registerAutoloader();
-
+	//Slim\Slim::registerAutoloader();
 
 	$app= new \Slim\Slim();
 	$app->config(array(
 		'debug' =>true ,
 		'templates.path' =>'Templates'));
+
+	$app-> map('/',function() use ($app){
+		
+		if(!isset($_SESSION['id_usuario'])){
+			//render login
+			$app->render('tmp_login.php');
+		}
+		else{
+			//enviar al inicio
+			$app->redirect($app->urlFor('PaginaInicio'));			
+		}	
+	})->via('GET')->name('Inicio');
 
 	?>
