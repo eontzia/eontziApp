b1d7d7e3e3389f95a54680605634dc31b7d9b7d0
Ierai Elizondo <ieraielizondo@gmail.com>
2015-12-17 08:24:41 +0100

    Revert "slim ok"
    This reverts commit faad91faf09727a8d7a7ab72f8bcac45e4611d7c.

--- app/index.php
+++ app/index.php
@@ -2,17 +2,17 @@
 	header ('Content-type: text/html; charset=utf-8');
 	session_start();
 
-	require 'vendor/autoload.php';	
+	require 'vendor/autoload.php';
 
-	Slim\Slim::registerAutoloader();
+	//Slim\Slim::registerAutoloader();
 
-	// create new Slim instance
-	$app = new \Slim\Slim();
+	$app= new \Slim\Slim();
 	$app->config(array(
 		'debug' =>true ,
 		'templates.path' =>'Templates'));
 
-	$app->get('/',function() use ($app){
+	$app-> map('/',function() use ($app){
+		
 		if(!isset($_SESSION['id_usuario'])){
 			//render login
 			$app->render('tmp_login.php');
@@ -21,8 +21,6 @@
 			//enviar al inicio
 			$app->redirect($app->urlFor('PaginaInicio'));			
 		}	
-	})->name('Inicio');
-
-	$app->run();
+	})->via('GET')->name('Inicio');
 
 	?>
