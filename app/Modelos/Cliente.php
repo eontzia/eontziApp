3474bb4f297b410b368308a503bfbe4ae06c4e72
Ierai Elizondo <ieraielizondo@gmail.com>
2015-12-18 13:49:09 +0100

    cambios varios

---
+++ app/Modelos/Cliente.php
@@ -0,0 +1,23 @@
+<?php
+header('Content-Type: text/html; charset=ISO-8859-1');
+include_once 'Control/BD/BD.php';
+include_once 'CorreoUser.php';
+require_once 'Utils.php';
+
+/**
+* CLASE Cliente
+*/
+class ClassName 
+{
+
+	function __construct()
+	{
+
+	}
+
+	public static function nuevoCliente(){
+
+	}
+}
+
+?>
