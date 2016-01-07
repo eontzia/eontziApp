73e7a82fc1d42c16cb3f61cdd9f742b37011cb64
ieraielizondo <ieraielizondo@gmail.com>
2016-01-07 02:06:13 +0100

    favicon added

--- app/Templates/info.php
+++ app/Templates/info.php
@@ -5,6 +5,9 @@
 	<link rel="stylesheet" type="text/css" href="../Templates/css/estilos.css">
 	<link rel="stylesheet" type="text/css" href="../../css/bootstrap.min.css">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0">
+	<link rel="icon" type="image/ico" href="../../img/favicon.ico"/>
+	<link rel="shortcut icon" href="../../img/favicon.ico"/>
+
 	<meta http-equiv="Content-Type" content="text/html"; charset="utf-8"/>
 	</head>
 <body>
--- app/Templates/tmp_login.php
+++ app/Templates/tmp_login.php
@@ -4,6 +4,8 @@
 	<title>Inicio trackingapp</title>
 	<link rel="stylesheet" type="text/css" href="Templates/css/estilos.css">
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+	<link rel="icon" type="image/ico" href="../img/favicon.ico"/>
+	<link rel="shortcut icon" href="../img/favicon.ico"/>
 	<meta http-equiv="Content-Type" content="text/html"; charset="utf-8"/>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0">
 	<script type="text/javascript" src="../js/jquery-1.11.3.min.js"></script>
